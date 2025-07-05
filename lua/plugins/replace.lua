db1751bb956574d2b4ad49468ba47bb80c6ad9d4
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-05 11:23:31 +0300

    🛠️ feat(plugin): Add GrugFar plugin for search & replace

---
+++ lua/plugins/replace.lua
@@ -0,0 +1,13 @@
+return {
+	"MagicDuck/grug-far.nvim",
+	opts = {},
+	keys = {
+		{
+			"<leader>sr",
+			"<CMD>GrugFarWithin<CR>",
+			desc = "Search & Replace",
+			noremap = true,
+			silent = true,
+		},
+	},
+}
