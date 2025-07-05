4a63d241563ed337947cd6ad7adc37b3929c1ec5
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-05 11:47:50 +0300

    ✨ feat(treesitter): Add treesitter-context plugin for context highlighting
    - Added new plugin `nvim-treesitter-context` for context highlighting with customizable settings.
    - Enabled by default with max_lines set to 1 and other configuration options.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,36 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  event = {
-    "BufEnter"
-  },
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,  
-    highlight = {
-      enable = true,
-      disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
-      additional_vim_regex_highlighting = true,
-    },
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufEnter" },
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+				disable = function(_, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+				additional_vim_regex_highlighting = true,
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = "BufReadPre",
+    enabled = false,
+		opts = {
+			enable = true, -- enable treesitter-context
+			max_lines = 1, -- no limit to context window
+			min_window_height = 0,
+			multiline_threshold = 20,
+			trim_scope = "outer",
+			mode = "cursor",
+			separator = nil,
+		},
+	},
 }
