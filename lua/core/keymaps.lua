fda031915d21881f4bb7ece331504e3fb252ad68
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-11 13:32:54 +0300

    ✨ feat(core/keymaps): add H keymap for first non-blank

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,7 +7,7 @@ vim.g.maplocalleader = "\\"
 ----------------------------------------
 --- Core Editing & Text Manipulation
 ----------------------------------------
-map("n", "<C-a>", "ggVG", { desc = "Select entire buffer", noremap = true, silent = true })
+-- map("n", "<C-a>", "ggVG", { desc = "Select entire buffer", noremap = true, silent = true })
 map("i", "<C-e>", "<C-o>dW", { desc = "Delete next word", noremap = true, silent = true })
 
 ----------------------------------------
@@ -57,6 +57,7 @@ map("n", "<C-d>", "<C-d>zz", vim.tbl_extend("force", nav_opts, { desc = "Scroll 
 map("n", "<C-u>", "<C-u>zz", vim.tbl_extend("force", nav_opts, { desc = "Scroll up centered" }))
 map("n", "n", "nzzzv", vim.tbl_extend("force", nav_opts, { desc = "Next match centered" }))
 map("n", "N", "Nzzzv", vim.tbl_extend("force", nav_opts, { desc = "Previous match centered" }))
+map("n", "H", "^", { noremap = true, silent = true, desc = "Go to first non-blank character" })
 
 ----------------------------------------
 --- Quality of Life Improvements
