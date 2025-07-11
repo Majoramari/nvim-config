9087cbaa21e8cdc2b2e05f99661f725a48ba2062
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-11 13:35:07 +0300

    🔧 chore(render-markdown): remove treesitter dependency

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,7 +1,7 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+	dependencies = { "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 	---@module 'render-markdown'
 	opts = {},
 }
