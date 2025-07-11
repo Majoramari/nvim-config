ce06ecbcb4e50a6328c0f66cce69009a9bd59d1c
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-11 13:33:28 +0300

    🎨 style(codesnap.nvim): remove watermark font family

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,20 +1,18 @@
 return {
-  "mistricky/codesnap.nvim",
-  build = "make",
-  opts = {
-    mac_window_bar = true,
-    title = "CodeSnap.nvim",
-    code_font_family = "JetBrainsMono Nerd Font",
-    watermark_font_family = "Pacifico",
-    watermark = "Made with love by Muhannad",
-    bg_theme = "default",
-    has_line_number = true,
-    min_width = 0,
-    bg_x_padding = 122,
-    bg_y_padding = 82
-  },
-  keys = {
-    -- Keymap Shift+s s
-    { "<S-s>s", ":CodeSnap<CR>", mode = "v", noremap = true, silent = true, desc = "Take a snapshot of your code"}
-  }
+	"mistricky/codesnap.nvim",
+	build = "make",
+	opts = {
+		mac_window_bar = true,
+		code_font_family = "JetBrainsMono Nerd Font",
+		-- watermark_font_family = "Pacifico",
+		watermark = "Made with love by Muhannad",
+		bg_theme = "default",
+		has_line_number = true,
+		min_width = 0,
+		bg_x_padding = 122,
+		bg_y_padding = 82,
+	},
+	keys = {
+		{ "<S-s>s", ":CodeSnap<CR>", mode = "v", noremap = true, silent = true, desc = "Take a snapshot of your code" },
+	},
 }
