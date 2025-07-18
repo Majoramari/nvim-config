862eec657b38265389afc6262e766c6e56e02b4b
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-18 03:08:26 +0300

    🔧 chore(lazy.nvim): update config to include cache and remove comments
    - Added `cache = { enabled = true }` to `performance.rtp`
    - Removed commented sections in `defaults` and `checker`

--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -1,56 +1,57 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Docs: https://lazy.folke.io/
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-    { import = "colorschemes" },
-  },
-  defaults = {
-    lazy = true,
-    version = "*",
-  },
-  -- install = {
-  --   colorscheme = { "catppuccin" },
-  --   -- Only install missing plugins, don't update
-  --   missing = true,
-  -- },
-  checker = {
-    enabled = true,
-    frequency = 604800,
-    notify = false,
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "rplugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-  ui = {
-    border = "rounded",
-  },
+	spec = {
+		{ import = "plugins" },
+		{ import = "colorschemes" },
+	},
+	defaults = {
+		lazy = true,
+		version = "*",
+	},
+	-- install = {
+	--   colorscheme = { "catppuccin" },
+	--   -- Only install missing plugins, don't update
+	--   missing = true,
+	-- },
+	checker = {
+		enabled = true,
+		frequency = 604800,
+		notify = false,
+	},
+	performance = {
+		rtp = {
+			cache = { enabled = true },
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"rplugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	ui = {
+		border = "rounded",
+	},
 })
