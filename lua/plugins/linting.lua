bf6fe8ee40eb5044f1c0c38cc702903d2451f0a6
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-18 03:11:42 +0300

    ✨ feat(linting): add dynamic linter configuration based on marker file
    - Add function to check if linting is disabled for a filetype
    - Dynamically apply linters based on marker file
    - Maintain existing autocmd for linting triggers

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,14 +1,12 @@
 local spec = { "mfussenegger/nvim-lint" }
 
 spec.enabled = true
-spec.lazy = true
-spec.event = { "BufReadPre", "BufNewFile" }
+spec.event = "VeryLazy"
 
 spec.config = function()
 	local lint = require("lint")
 
-	-- specify linters per filetype
-	lint.linters_by_ft = {
+	local default_linters_by_ft = {
 		lua = { "selene" },
 		cmake = { "cmakelint" },
 		html = { "markuplint" },
@@ -16,6 +14,24 @@ spec.config = function()
 		go = { "golangcilint" },
 	}
 
+	-- Check if linting is disabled for this filetype
+	local function linting_disabled_for_ft(ft)
+		local marker = vim.fn.getcwd() .. "/.disable-lint-" .. ft
+		return vim.fn.filereadable(marker) == 1
+	end
+
+	-- Dynamically apply linters based on marker
+	lint.linters_by_ft = setmetatable({}, {
+		__index = function(_, ft)
+			if linting_disabled_for_ft(ft) then
+				return {}
+			else
+				return default_linters_by_ft[ft] or {}
+			end
+		end,
+	})
+
+	-- Autocmd to trigger linting
 	local lint_augroup = vim.api.nvim_create_augroup("nvim-lint", { clear = true })
 
 	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
