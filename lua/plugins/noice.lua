ee3ba07f11baeeeb2b8001b746a3695909f9180a
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-18 03:33:35 +0300

    ✨ feat(noice): add dependencies and key mappings
    - Add new plugins and configure nvim-notify
    - Move keys section under opts
    - Add dismiss and history key mappings

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,6 +1,10 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{ "rcarriga/nvim-notify", opts = { render = "compact", stages = "slide" } },
+	},
 	opts = {
 		-- lsp = {
 		-- 	documentation = {
@@ -18,17 +22,25 @@ return {
 		-- 		},
 		-- 	},
 		-- },
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			{ "rcarriga/nvim-notify", opts = { render = "compact" } },
+	},
+	keys = {
+		{
+			"<localleader>nn",
+			"<cmd>NoiceDismiss<CR>",
+			silent = true,
+			desc = "Dismiss notification",
 		},
-		keys = {
-			{
-				"<leader>nd",
-				"<cmd>NoiceDismiss<CR>",
-				silent = true,
-				desc = "Dismiss notification",
-			},
+		{
+			"<localleader>nh",
+			"<cmd>NoiceHistory<CR>",
+			silent = true,
+			desc = "Show notifications history",
+		},
+		{
+			"<localleader>nl",
+			"<cmd>NoiceHistory<CR>",
+			silent = true,
+			desc = "Show last notifications",
 		},
 	},
 }
