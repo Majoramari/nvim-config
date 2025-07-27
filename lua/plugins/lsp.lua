af88615376b9ecfd42181cdc0f18f251d40c5cac
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-07-27 13:09:19 +0300

    ♻️ refactor(lsp.lua): restructure LSP config for keymaps and diagnostics
    - Removed mason-lspconfig, now using cmp-nvim-lsp and file-operations plugin
    - Added keymaps for LSP actions (definition, declaration, etc.)
    - Configured diagnostic signs and underline

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,149 +1,170 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{
-				"williamboman/mason.nvim",
-				build = ":MasonUpdate",
-				config = true,
-			},
-		},
-		opts = {
-			automatic_installation = true,
-			ensure_installed = {
-				"lua_ls",
-				"biome",
-				"clangd",
-				"cmake",
-				"html",
-				"cssls",
-				"emmet_ls",
-				"zls",
-				"bashls",
-				"gopls",
-			},
-			handlers = {
-				function(server_name)
-					local nvim_lsp = require("lspconfig")
-					local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-					if server_name == "clangd" then
-						nvim_lsp.clangd.setup({
-							capabilities = capabilities,
-							cmd = {
-								"clangd",
-								"--background-index",
-								"--suggest-missing-includes",
-								"--header-insertion=never",
-								"--compile-commands-dir=build",
-								"--clang-tidy",
-								"--cache=.out/.cache/clangd",
-							},
-						})
-					else
-						nvim_lsp[server_name].setup({
-							capabilities = capabilities,
-						})
-					end
-				end,
-			},
-		},
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
 	},
+	config = function()
+		-- NOTE: LSP Keybinds
 
-	-- Better UI for vim.ui.select, vim.ui.input, etc.
-	{
-		"stevearc/dressing.nvim",
-		event = "VeryLazy",
-		opts = {},
-	},
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
+				local map = vim.keymap.set
 
-	-- Neovim LSP Config: Configures LSP servers
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"aznhe21/actions-preview.nvim",
-			"MysticalDevil/inlay-hints.nvim",
-			{
-				"folke/lazydev.nvim",
-				ft = "lua",
-				opts = {
-					library = {
-						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-					},
+				-- keymaps
+
+				opts.desc = "Show LSP definitions"
+				map("n", "gd", vim.lsp.buf.definition, opts)
+
+				opts.desc = "Go to declaration"
+				map("n", "gD", vim.lsp.buf.declaration, opts)
+
+				opts.desc = "Show LSP implementations"
+				map("n", "gi", vim.lsp.buf.implementation, opts)
+
+				opts.desc = "Show LSP type definitions"
+				map("n", "gt", vim.lsp.buf.type_definition, opts)
+
+				opts.desc = "Show LSP references"
+				map("n", "gr", vim.lsp.buf.references, opts)
+
+				opts.desc = "See available code actions"
+				map({ "n", "v" }, "gf", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+
+				opts.desc = "Smart rename"
+				map("n", "<F2>", vim.lsp.buf.rename, opts)
+
+				opts.desc = "Show documentation for what is under cursor"
+				map("n", "K", vim.lsp.buf.hover, opts)
+			end,
+		})
+
+		-- Define sign icons for each severity
+		local signs = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = "󰠠 ",
+			[vim.diagnostic.severity.INFO] = " ",
+		}
+
+		-- Set the diagnostic config with all icons
+		vim.diagnostic.config({
+			signs = {
+				text = signs, -- Enable signs in the gutter
+			},
+			virtual_text = false, -- Specify Enable virtual text for diagnostics
+			underline = true, -- Specify Underline diagnostics
+			update_in_insert = false, -- Keep diagnostics active in insert mode
+		})
+
+		-- Setup servers
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		-- lua_ls
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
 					diagnostics = {
-						signs = false,
-						underline = false,
-						update_in_insert = false,
-						severity_sort = true,
-						float = {
-							border = "rounded",
-							source = "always",
+						globals = { "vim" },
+					},
+					completion = {
+						callSnippet = "Replace",
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.stdpath("config") .. "/lua"] = true,
 						},
 					},
 				},
 			},
-		},
-		config = function()
-			vim.diagnostic.config({
-				underline = false,
-				virtual_text = false,
-				update_in_insert = false,
-				severity_sort = true,
-				float = {
-					boarder = "single",
-				},
-				signs = {
-					text = {
-						[vim.diagnostic.severity.ERROR] = "",
-						[vim.diagnostic.severity.WARN] = "",
-						[vim.diagnostic.severity.HINT] = "",
-						[vim.diagnostic.severity.INFO] = "",
-					},
-				},
-			})
+		})
 
-			-- LSP key mappings on attach
-			vim.api.nvim_create_autocmd("LspAttach", {
-				desc = "LSP actions",
-				callback = function(event)
-					local buf = event.buf
-					local map = vim.keymap.set
+		-- emmet_language_server
+		lspconfig.emmet_language_server.setup({
+			capabilities = capabilities,
+			filetypes = {
+				"css",
+				"eruby",
+				"html",
+				"javascript",
+				"javascriptreact",
+				"less",
+				"sass",
+				"scss",
+				"pug",
+				"typescriptreact",
+			},
+			init_options = {
+				includeLanguages = {},
+				excludeLanguages = {},
+				extensionsPath = {},
+				preferences = {},
+				showAbbreviationSuggestions = true,
+				showExpandedAbbreviation = "always",
+				showSuggestionsAsSnippets = false,
+				syntaxProfiles = {},
+				variables = {},
+			},
+		})
 
-					map("n", "K", function()
-						vim.lsp.buf.hover({ border = "single" })
-					end)
-					map("n", "gd", vim.lsp.buf.definition, { buffer = buf, desc = "Go to Definition" })
-					map("n", "gD", vim.lsp.buf.declaration, { buffer = buf, desc = "Go to Declaration" })
-					map("n", "gi", vim.lsp.buf.implementation, { buffer = buf, desc = "Go to Implementation" })
-					map("n", "go", vim.lsp.buf.type_definition, { buffer = buf, desc = "Go to Type Definition" })
-					map("n", "gr", vim.lsp.buf.references, { buffer = buf, desc = "Find References" })
-					map("n", "ge", function()
-						vim.diagnostic.open_float(nil, { border = "single" })
-					end, { buffer = buf, desc = "Show Diagnostic" })
-					map("n", "<F2>", vim.lsp.buf.rename, { buffer = buf, desc = "Rename" })
-				end,
-			})
-		end,
-		keys = {
-			{
-				"gf",
-				function()
-					require("actions-preview").code_actions()
-				end,
-				desc = "Show Code Actions",
-				silent = true,
+		-- -- denols
+		-- lspconfig.denols.setup({
+		-- 	capabilities = capabilities,
+		-- 	root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+		-- })
+
+		-- ts_ls (replaces tsserver)
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+			root_dir = function(fname)
+				local util = lspconfig.util
+				return not util.root_pattern("deno.json", "deno.jsonc")(fname)
+					and util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git")(fname)
+			end,
+			single_file_support = false,
+			init_options = {
+				preferences = {
+					includeCompletionsWithSnippetText = true,
+					includeCompletionsForImportStatements = true,
+				},
 			},
-			{
-				"<leader>i",
-				function()
-					vim.cmd("InlayHintsToggle")
-				end,
-				desc = "Toggle Inlay Hints",
-				silent = true,
+		})
+
+		local default_opt = {
+			capabilities = capabilities,
+		}
+
+		lspconfig.lua_ls.setup(default_opt)
+		lspconfig.biome.setup(default_opt)
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+			cmd = {
+				"clangd",
+				"--background-index",
+				"--suggest-missing-includes",
+				"--header-insertion=never",
+				"--compile-commands-dir=build",
 			},
-		},
-	},
+		})
+		lspconfig.cmake.setup(default_opt)
+		lspconfig.html.setup(default_opt)
+		lspconfig.angularls.setup({
+			capabilities = capabilities,
+			root_dir = lspconfig.util.root_pattern("angular.json"),
+		})
+		lspconfig.cssls.setup(default_opt)
+		lspconfig.zls.setup(default_opt)
+		lspconfig.bashls.setup(default_opt)
+		lspconfig.gopls.setup(default_opt)
+		lspconfig.pyright.setup(default_opt)
+	end,
 }
