cf9b6378b42c2d02a7d8a2e2afc409845403b92f
Muhannad Elbolaky <think-basin-amber@duck.com>
2025-08-01 12:04:46 +0300

    ♻️ refactor(formatting): Add new formatters and restructure keymap
    - Updated formatters_by_ft with python, javascript, and biome-check
    - Refactored keymap setup to use vim.keymap.set
    - Organized configuration into a config function

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,28 +1,31 @@
 return {
 	"stevearc/conform.nvim",
 	event = "VeryLazy",
-	opts = {
-		formatters_by_ft = {
-			lua = { "stylua" },
-			cpp = { "clang-format" },
-			cmake = { "gersemi" },
-			html = { "prettierd" },
-			css = { "prettierd" },
-			bash = { "shfmt" },
-			go = { "goimports", "gofumpt" },
-		},
-	},
-	keys = {
-		{
-			"<C-f>",
-			function()
-				require("conform").format({
-					lsp_format = "fallback",
-					async = false,
-					timeout_ms = 500,
-				})
-			end,
-			{ mode = { "n", "x" }, noremap = true, silent = true, desc = "Format buffer" },
-		},
-	},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				cpp = { "clang-format" },
+				cmake = { "gersemi" },
+				html = { "prettierd" },
+				bash = { "shfmt" },
+				python = { "ruff" },
+				go = { "goimports", "gofumpt" },
+				["javascript"] = { "biome-check" },
+				["javascriptreact"] = { "biome-check" },
+				["typescript"] = { "biome-check" },
+				["typescriptreact"] = { "biome-check" },
+				["json"] = { "biome-check" },
+				["css"] = { "biome-check" },
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>lf", function()
+			require("conform").format({
+				lsp_format = "fallback",
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { noremap = true, silent = true, desc = "Format buffer" })
+	end,
 }
